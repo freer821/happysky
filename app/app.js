d99f79f4e82202105b0ee5dd2d77cc56b8da68fd
freer821 <freergzy8212@hotmail.com>
2017-04-01 15:31:24 +0200

    init app

---
+++ app/app.js
@@ -0,0 +1,31 @@
+var express = require("express"),
+    app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+
+app.use('/scripts', express.static(path.join(__dirname, '../node_modules')));
+app.use('/libs', express.static(path.join(__dirname, '../libs')));
+app.use('/data', express.static(path.join(__dirname, '../data')));
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/view/index.html');
+});
+	
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('danmaku', (comment) => {
+    io.emit('danmaku', comment);
+  });
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
