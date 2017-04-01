728e9612291fa23ea1ea41a22ad43df6bbadb2c0
freer821 <freergzy8212@hotmail.com>
2017-04-01 21:02:27 +0200

    bugfixing

--- app/server.js
+++ app/server.js
@@ -6,8 +6,7 @@ var path = require('path');
 
 
 app.use('/scripts', express.static(path.join(__dirname, '../node_modules')));
-app.use('/reactlib', express.static(path.join(__dirname, '../libs/react')));
-app.use('/data', express.static(path.join(__dirname, '../data')));
+app.use('/reactlib', express.static(path.join(__dirname, './libs/react')));
 
 
 app.get('/', function(req, res){
--- app/view/controll/App.jsx
+++ app/view/controll/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-         <video id="video" className="video-js" controls preload="auto" width="640" height="264" data-setup="{}"></video>
+          <h1>Hallo World </h1>
       );
    }
 }
