828b57b9b47fea227b5a8aefc68bde72c65fc52d
freer821 <freergzy8212@hotmail.com>
2017-04-02 13:49:47 +0200

    change text

--- app/view/react/App.jsx
+++ app/view/react/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-          <h1>Hallo World </h1>
+          <h1>Welcome to  Happy Sky</h1>
       );
    }
 }
